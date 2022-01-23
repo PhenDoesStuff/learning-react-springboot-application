0e36adb24f33d415add64c3f9d803a349bbbecbb
Stephen Montague <stephen.d.montgue@gmail.com>
2022-01-22 23:02:49 -0500

    Finished our service that will get information from our backend and display it on our frontend

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_API_URL = 'http://localhost:8080/';
-const EMPLOYEES = 'employees';
+const BASE_API_URL = 'http://localhost:8080/api/v1';
+const EMPLOYEES = '/employees';
 
 class EmployeeService {
 	getAllEmployees() {
