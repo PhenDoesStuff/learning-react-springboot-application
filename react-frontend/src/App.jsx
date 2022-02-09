c4575d175e5430a377c3477dceda66b3f3691121
Stephen Montague <stephen.montague@deposco.com>
2022-02-08 21:57:19 -0500

    Creation of Header and Footer component.

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import { EmployeeList } from './components/EmployeeList';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<EmployeeList />
+			<Footer />
 		</div>
 	);
 }
