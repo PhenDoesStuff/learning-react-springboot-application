bfa0fabc8c63e1b4a1c1dc688b52efd04bf1477f
Stephen Montague <stephen.montague@deposco.com>
2022-02-08 22:09:57 -0500

    Using React Router Dom to have routes for the program.

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { EmployeeList } from './components/EmployeeList';
 import Footer from './components/Footer';
 import Header from './components/Header';
@@ -6,9 +7,16 @@ import Header from './components/Header';
 function App() {
 	return (
 		<div>
+			<Router>
 			<Header />
-			<EmployeeList />
+			<div className="container">
+				<Routes>
+					<Route exact path='/' element={<EmployeeList/>}></Route>
+					<Route path='/employees' element={<EmployeeList/>}></Route>
+				</Routes>
+			</div>
 			<Footer />
+			</Router>
 		</div>
 	);
 }
