19e9e00cead0b148f90f2bd5d025d6d3810f7c7e
Stephen Montague <stephen.montague@deposco.com>
2022-02-12 22:14:08 -0500

    Created a get employee by id api endpoint

--- springboot-backend/src/main/java/com/montaguestephen/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/montaguestephen/springboot/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.montaguestephen.springboot.controller;
 
+import com.montaguestephen.springboot.exception.ResourceNotFoundException;
 import com.montaguestephen.springboot.model.Employee;
 import com.montaguestephen.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,9 +22,17 @@ public class EmployeeController  {
         return employeeRepository.findAll();
     }
 
-    // build create employee REST API
+    // Build Create (POST) Employees REST API
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // Build GET Employee by ID REST API
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee with id " + id + " does not exists"));
+        return ResponseEntity.ok(employee);
     }
 }
